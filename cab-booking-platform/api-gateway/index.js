a0d6272f8a95544f51e978482cb4816053affe70
Bonitt <matthewbonett1309@gmail.com>
2026-05-17 17:03:39 +0200

    created the API gateway for the microservices

---
+++ cab-booking-platform/api-gateway/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+app.use(cors());
+
+
+app.use('/api/customers', createProxyMiddleware({ target: 'http://localhost:3001/api/customers', changeOrigin: true }));
+app.use('/api/bookings', createProxyMiddleware({ target: 'http://localhost:3002/api/bookings', changeOrigin: true }));
+app.use('/api/payments', createProxyMiddleware({ target: 'http://localhost:3003/api/payments', changeOrigin: true }));
+app.use('/api/locations', createProxyMiddleware({ target: 'http://localhost:3004/api/locations', changeOrigin: true }));
+
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'API Gateway is healthy' });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`API Gateway is running on port ${PORT}`);
+});
